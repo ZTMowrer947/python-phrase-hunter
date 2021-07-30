0efc1f73b65ee4fdde5f71560959e817a1eee4c6
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-07-30 15:43:52 -0400

    Write psuedocode for Phrase and Game classes

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,1 +1,71 @@
+"""Module for Game class"""
 # Create your Game class logic in here.
+
+"""
+class Game
+    set TOTAL_LIVES property on class to 5
+
+    define initializer
+        set phrases property on instance to list of Phrase instances each with predetermined phrases
+        set missed property on instance to 0
+        set active_phrase property on instance to None
+        set guesses property on instance to an empty list
+    end initializer
+    
+    define method get_random_phrase
+        return randomly selected phrase from instance phrases list
+    end method
+    
+    define method welcome
+        print "Welcome to Phrase Hunter! Can you guess the phrase before you run out of lives?"
+    end method
+    
+    define method get_guess
+        while user guess is not valid
+            prompt user for guess
+            store result in user_guess
+            
+            if user_guess is not one character or is not a letter
+                print "Sorry, that isn't a valid guess. Guesses must consist of single letters."
+                loop so that user can make another guess
+            else if user_guess is in instance guesses list
+                print "You have already guessed that letter."
+                loop so that user can make another guess
+            else
+                append user_guess to instance guesses list
+                return user_guess
+            end if
+        end while
+    end method
+    
+    define method game_over
+        set message
+            to "Congratulations, you win!" if result of calling check_complete on instance active_phrase is True
+            to "Sorry, you lost..." otherwise
+        end set
+        
+        print message
+    end method
+    
+    define method start
+        call welcome method
+        
+        set instance active_phrase to result of calling get_random_phrase method
+        
+        while instance missed property is less than class TOTAL_LIVES property
+            and result of calling check_complete on instance active_phrase is False
+            
+            call display method on instance active_phrase
+            
+            set guess to result of calling get_guess method
+            
+            if result of calling check_letter on instance active_phrase is False
+                increment instance missed by 1
+                print "Incorrect. You have {missed} out of {TOTAL_LIVES} remaining."
+            end if
+        end while
+        
+        call method game_over
+    end method
+end class
+"""
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,48 @@
+"""Module for Phrase class"""
 # Create your Phrase class logic here.
+
+"""
+class Phrase
+    define initializer
+        with arg phrase of type string
+        
+        set phrase property on instance to lowercased phrase
+        set correct_guesses property to empty list
+    end initializer
+    
+    define method display
+        with arg correct_guesses of type list
+        set split_string to instance phrase converted to list
+        
+        for each index and char in enumerating split_string
+            if char is not in guesses
+                set char at index to "_"
+            end if
+        end for
+        
+        set rejoined_string to split_string rejoined by ""
+        print rejoined_string
+    end method
+    
+    define method check_letter
+        with arg letter of type string
+        
+        set is_correct to result of checking if letter is in instance phrase
+        
+        if is_correct
+            append letter to instance correct_guesses list
+        end if
+        
+        return is_correct
+    end method
+    
+    define method check_complete
+        with arg guesses of type list
+        
+        set letters to instance phrase converted to set
+        set guesses_set to guesses converted to set
+        
+        return result of checking if letters is a subset of guesses_set
+    end method    
+end class
+"""
