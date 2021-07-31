6720e18e363db588f0881099ada465912a709c9f
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-07-31 08:30:17 -0400

    Setup app to run game

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-# Import your Game class
+"""Main application"""
+from phrasehunter.game import Game
 
-# Create your Dunder Main statement.
+if __name__ == '__main__':
+    game = Game()
 
-# Inside Dunder Main:
-## Create an instance of your Game class
-## Start your game by calling the instance method that starts the game loop
+    game.start()
