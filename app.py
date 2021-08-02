2aac82ee5c9194c524585c30dfb013a6f4708c78
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-08-02 13:07:42 -0400

    Modify game to be replayable

--- app.py
+++ app.py
@@ -2,6 +2,16 @@
 from phrasehunter.game import Game
 
 if __name__ == '__main__':
-    game = Game()
+    should_keep_playing = True
 
-    game.start()
+    while should_keep_playing:
+        # Initialize a new game every time
+        game = Game()
+
+        game.start()
+
+        # Continue playing games until user explicitly opts to stop
+        should_keep_playing = input("\nPlay again? (Y/n) ").lower() != "n"
+        print()
+    else:
+        print("Thanks for playing.")
